e9536f43d8b4315d50fd3521f45c3bf761526f39
Szirják Csaba <github@aplyr.com>
2024-04-23 11:29:58 +0200

    Update competitorName.tsx

--- web/src/pages/components/competitorName.tsx
+++ web/src/pages/components/competitorName.tsx
@@ -15,12 +15,17 @@ export default function CompetitorName() {
   if (!competitor) return "loading...";
 
   return (
-    <>
+    <div className="flex gap-2 items-center">
       <Avatar className="h-8 w-8">
         <AvatarImage src={competitor.photoUrl} />
       </Avatar>
 
       <span className="text-sm font-bold">{competitor.name}</span>
-    </>
+      {competitor.workstation?.length > 0 && (
+        <span className="text-xs text-gray-300">
+          ({competitor.workstation})
+        </span>
+      )}
+    </div>
   );
 }
