f53095faebf40770df069c71d6d4f0a33016c5d4
Bence Lakos <bencelakos@gmail.com>
2024-04-24 08:29:25 +0200

    Add docker support

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,10 +2,7 @@ import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
 import path from "path";
-
-dotenv.config();
 
 const app: Express = express();
 app.use(cors());
